6b8eb6078ca843236696d4b09c9f84ed61d2b4de
yhz3 <105172127+yhz3@users.noreply.github.com>
2025-03-19 18:19:22 -0400

    adding test version of showing previous edits

--- frontend/src/componentsStatic/scoring/DatabaseSentenceView.tsx
+++ frontend/src/componentsStatic/scoring/DatabaseSentenceView.tsx
@@ -3,11 +3,126 @@ import "../../index.css";
 import sentences from "../../../public/mandarin_dataset.json";
 import checkmark from "../../assets/checkmark.svg";
 import cross from "../../assets/x_cross.svg";
-
+import { generateDiff } from "../postEdit/PostEditContainer"
 // Type Definitions
 type DatasetType = {
   mandarin_dataset: any[];
   cantonese_dataset: any[];
+};
+
+const dummy_data = {
+  "_id": "67b69d6870364bd2b0e04e14",
+  "id": 1,
+  "src": "On Monday, scientists from the Stanford University School of Medicine announced the invention of a new diagnostic tool that can sort cells by type: a tiny printable chip that can be manufactured using standard inkjet printers for possibly about one U.S. cent each.",
+  "mt": "星期一,斯坦福大學醫學院嘅科學家宣布發明咗一個可以按類型排序細胞嘅新診斷工具 一個細嘅可打印芯片 可以用標準嘅喷墨打印機 製造成一美分鐘",
+  "ref": "星期一，史丹福大學醫學院嘅科學家宣佈，佢哋發明咗一種可以對細胞進行分類嘅新診斷工具：用標準噴墨打印機製造嘅微型可打印晶片，每塊晶片嘅價格大概係 1 美仙。",
+  "annotations": {
+    "ethan": {
+      "cheung@mail": {
+        "utoronto": {
+          "ca_annotations": {
+            "annotatedSpans": [],
+            "overall_translation_score": 50,
+            "corrected_sentence": "星期一,斯坦福大學醫學院嘅科學家宣布發明咗一個可以按類型排序細胞嘅新診斷工具 一個細嘅可打印芯片 可以用標準嘅喷墨打印機 製造成一美分鐘"
+          }
+        }
+      }
+    },
+    "Mekael_annotations": {
+      "annotatedSpans": [
+        {
+          "error_text_segment": "細嘅",
+          "start_index": 41,
+          "end_index": 43,
+          "error_type": "Addition",
+          "error_severity": "Minor"
+        }
+      ],
+      "overall_translation_score": 50,
+      "corrected_sentence": "星期一,斯坦福大學醫學院嘅科學家宣布發明咗一個可以按類型排序細胞嘅新診斷工具 一個細嘅可打印芯片 可以用標準嘅喷墨打印機 製造成一美分鐘"
+    },
+    "loka9_annotations": {
+      "annotatedSpans": [
+        {
+          "error_text_segment": "，",
+          "start_index": 3,
+          "end_index": 4,
+          "error_type": "Grammar",
+          "error_severity": "Minor"
+        },
+        {
+          "error_text_segment": "佢哋",
+          "start_index": 18,
+          "end_index": 20,
+          "error_type": "Omission",
+          "error_severity": "Minor"
+        },
+        {
+          "error_text_segment": "對細胞進行分類",
+          "start_index": 27,
+          "end_index": 34,
+          "error_type": "Mistranslation",
+          "error_severity": "Minor"
+        },
+        {
+          "error_text_segment": "用標準噴墨打印機製造嘅微型可打印晶片，每塊晶片嘅價格大概係 1 美仙。 \n",
+          "start_index": 43,
+          "end_index": 80,
+          "error_type": "Mistranslation",
+          "error_severity": "Minor"
+        }
+      ],
+      "overall_translation_score": 50,
+      "corrected_sentence": "星期一，斯坦福大學醫學院嘅科學家宣布佢哋發明咗一個可以對細胞進行分類嘅新診斷工具：一個用標準噴墨打印機製造嘅微型可打印晶片，每塊晶片嘅價格大概係 1 美仙。 \n\n\n\n"
+    },
+    "Phantom65536_annotations": {
+      "annotatedSpans": [
+        {
+          "error_text_segment": "成",
+          "start_index": 63,
+          "end_index": 64,
+          "error_type": "Omission",
+          "error_severity": "Minor"
+        }
+      ],
+      "overall_translation_score": 50,
+      "corrected_sentence": "星期一,斯坦福大學醫學院嘅科學家宣布發明咗一個可以按類型排序細胞嘅新診斷工具 一個細嘅可打印芯片 可以用標準嘅喷墨打印機 製造成本為一美仙"
+    },
+    "wingspecialist_annotations": {
+      "annotatedSpans": [
+        {
+          "error_text_segment": "史丹福",
+          "start_index": 4,
+          "end_index": 7,
+          "error_type": "Mistranslation",
+          "error_severity": "Minor"
+        },
+        {
+          "error_text_segment": "佢係",
+          "start_index": 39,
+          "end_index": 41,
+          "error_type": "Addition",
+          "error_severity": "Minor"
+        },
+        {
+          "error_text_segment": "噴",
+          "start_index": 57,
+          "end_index": 58,
+          "error_type": "Spelling",
+          "error_severity": "Minor"
+        },
+        {
+          "error_text_segment": "每塊大約價值一美金",
+          "start_index": 65,
+          "end_index": 74,
+          "error_type": "Mistranslation",
+          "error_severity": "Minor"
+        }
+      ],
+      "overall_translation_score": 50,
+      "corrected_sentence": "星期一,史丹福大學醫學院嘅科學家宣布發明咗一個可以按類型排序細胞嘅新診斷工具。佢係一個細嘅可打印芯片，可以用標準嘅噴墨打印機製造，每塊大約價值一美金"
+    }
+  }
 };
 
 type DatabaseSentenceViewProps = {
@@ -85,6 +200,48 @@ export const DatabaseSentenceView: React.FC<DatabaseSentenceViewProps> = ({
     setModifedText(item.mt);
     setAddedErrorSpans([]);
     setHighlightedError([]);
+
+    if (`${username}_annotations` in item.annotations) {
+      console.log("user has done this annotation already, loading previously submitted annotation");
+      const prev_annotation = item.annotations[`${username}_annotations`];
+      console.log("previous annotation data:");
+      console.log(prev_annotation);
+      // setDiffContent(prev_annotation.corrected_sentence);
+      // setModifedText(prev_annotation.corrected_sentence);
+      // setAddedErrorSpans(prev_annotation);
+      // setHighlightedError(prev_annotation);
+
+    }
+
+    // "Phantom65536_annotations": {
+    //   "annotatedSpans": [
+    //     {
+    //       "error_text_segment": "成",
+    //       "start_index": 63,
+    //       "end_index": 64,
+    //       "error_type": "Omission",
+    //       "error_severity": "Minor"
+    //     }
+    //   ],
+    //   "overall_translation_score": 50,
+    //   "corrected_sentence": "星期一,斯坦福大學醫學院嘅科學家宣布發明咗一個可以按類型排序細胞嘅新診斷工具 一個細嘅可打印芯片 可以用標準嘅喷墨打印機 製造成本為一美仙"
+    // },
+
+    const dummy = dummy_data.annotations.Phantom65536_annotations;
+    console.log("previous annotation data:", dummy);
+    setDiffContent(dummy.corrected_sentence);
+    setModifedText(dummy.corrected_sentence);
+    console.log("spans before mapping:", dummy.annotatedSpans);
+    const modified_dummy_spans = dummy.annotatedSpans.map(span => ({
+      ...span,
+      original_text: span.error_text_segment,
+      start_index_translation: span.start_index,
+      end_index_translation: span.end_index,
+    }));
+    console.log("new spans:", modified_dummy_spans);
+    setAddedErrorSpans(modified_dummy_spans);
+    setHighlightedError(modified_dummy_spans);
+    generateDiff(dummy_data.mt, dummy.corrected_sentence, setModifedText, setDiffContent);
   };
 
   const handleDatabaseFetch = async (
